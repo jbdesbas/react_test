6fd18eb25bc75e7024966729fb0619d4eeaabe5a
Jean-Baptiste DESBAS <jean-baptiste.desbas@hautsdefrance.fr>
2023-06-23 09:42:44 +0200

    fix map

--- src/components/multi-select.js
+++ src/components/multi-select.js
@@ -15,20 +15,17 @@ function WfsSelect(){
             fetch(`${wfs_endpoint}?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=${layername}&OUTPUTFORMAT=application%2Fjson&&srsName=EPSG:4326&CQL_FILTER=${search_field}%20ilike%20%27${filterText}%25%27`).then( response => {
                 return response.json();
                 }
-            )
-            .then(data => {
-              const data2 = data.map(c => ({value:c, label:c.properties[search_field]}) );
+            ).then(data => {
+              const data2 = data.features.map(c => ({value:c.properties.code, label:c.properties[search_field]}) );
               resolve(data2);
             })
             .catch(error => {
+                console.log(error);
                 reject(error);
               });
         })
-      
       }
     
-
-
       return (
         <AsyncSelect loadOptions={getOptions} />
   );
